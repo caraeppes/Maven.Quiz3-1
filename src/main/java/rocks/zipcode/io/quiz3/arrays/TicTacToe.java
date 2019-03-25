8c93d9b448eeb8bf3afcde00e9bc4675774acbe2
Cara Eppes <ceppes@udel.edu>
2019-03-25 10:38:08 -0400

    completed arrays

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -5,33 +5,60 @@ package rocks.zipcode.io.quiz3.arrays;
  */
 public class TicTacToe {
 
+    String[][] board;
+
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        return board[value];
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        String[] column = new String[3];
+        for(int i = 0; i < 3; i++){
+            column[i] = getRow(i)[value];
+        }
+        return column;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+        return getRow(rowIndex)[0].equals(getRow(rowIndex)[1]) && getRow(rowIndex)[0].equals(getRow(rowIndex)[2]);
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        return getColumn(columnIndex)[0].equals(getColumn(columnIndex)[1]) &&
+                getColumn(columnIndex)[0].equals(getColumn(columnIndex)[2]);
+    }
+
+    public Boolean isTopToBottomDiagonalHomogenous(){
+        return board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]);
+    }
+
+    public Boolean isBottomToTopDiagonalHomogenous(){
+        return board[2][0].equals(board[1][1]) && board[1][1].equals(board[0][2]);
     }
 
     public String getWinner() {
-        return null;
+        for(int i = 0; i < 3; i++){
+            if(isRowHomogenous(i)){
+                return board[i][0];
+            }
+            if(isColumnHomogeneous(i)){
+                return getColumn(i)[0];
+            }
+            if(isTopToBottomDiagonalHomogenous() || isBottomToTopDiagonalHomogenous()) {
+                return board[1][1];
+            }
+        }
+        return "";
     }
 
     public String[][] getBoard() {
-        return null;
+        return board;
     }
 }
